d9baf8051fbab3a63d6ae2e1d7b0262d6d228741
popiol <opiola.piotr@gmail.com>
2021-01-08 09:49:42 +0100

    creat plots in a loop

--- js/main.js
+++ js/main.js
@@ -1,40 +1,41 @@
+function create_line_plot(id) {
+	return function(data) {
+		var chart = new ApexCharts(document.querySelector("#"+id), {
+			chart: {
+				type: 'line',
+				width: '99%',
+				height: 400
+			},
+			series: [{
+				name: 'portfolio',
+				data: data.map(x => x.val)
+			}, {
+				name: 'market',
+				data: data.map(x => x.market_val)	
+			}],
+			xaxis: {
+				type: 'datetime',
+				categories: data.map(x => x.session_dt.split(' ')[0]),
+				labels: {
+					datetimeFormatter: {
+						month: 'MMM'
+					}
+				}
+			}
+		});
+		chart.render();
+		year = id.replace('stocks_line_vs_market_','');
+		$('#profit'+year).text(Math.round(data[data.length-1].val));
+		$('#market'+year).text(Math.round(data[data.length-1].market_val));
+	}
+}
+
+
 $(function(){
 	$('.stocks_line_vs_market').each(function(){
 		id = this.id;
-		alert(id);
 		filename = "data/"+id.replace('stocks_','')+'.csv';
-		d3.csv(filename+"?r="+Math.random(),
-			function(data) {
-				var chart = new ApexCharts(document.querySelector("#"+id), {
-					chart: {
-						type: 'line',
-						width: '99%',
-						height: 400
-					},
-					series: [{
-						name: 'portfolio',
-						data: data.map(x => x.val)
-					}, {
-						name: 'market',
-						data: data.map(x => x.market_val)	
-					}],
-					xaxis: {
-						type: 'datetime',
-						categories: data.map(x => x.session_dt.split(' ')[0]),
-						labels: {
-							datetimeFormatter: {
-								month: 'MMM'
-							}
-						}
-					}
-				});
-				chart.render();
-				year = id.replace('stocks_line_vs_market_','');
-				alert(year);
-				$('#profit'+year).text(Math.round(data[data.length-1].val));
-				$('#market'+year).text(Math.round(data[data.length-1].market_val));
-			}
-		)
+		d3.csv(filename+"?r="+Math.random()).then(create_line_plot(id))
 	});
 
 	best_worst_scale = {x:1,y:1,xx:.7,yy:.3,done:0,rescale(){
